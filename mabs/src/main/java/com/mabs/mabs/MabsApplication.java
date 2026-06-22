5bd9ca8d17ab7b321e1bf057bc21ba912c86696c
anhsnkaa <anhsnkaa@gmail.com>
2026-06-22 22:40:12 +0700

    config

---
+++ mabs/src/main/java/com/mabs/mabs/MabsApplication.java
@@ -0,0 +1,13 @@
+package com.mabs.mabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabsApplication.class, args);
+	}
+
+}
---
+++ mabs/src/test/java/com/mabs/mabs/MabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabs.mabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
