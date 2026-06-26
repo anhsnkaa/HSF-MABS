5c0a4e7ea471619ab11ce5978e808674f6d67d05
QuynhNTT1802 <nt.thuyquynh1802@gmail.com>
2026-06-26 22:42:26 +0700

    tao lai project

---
+++ MedicalAppointmentBookingSystem/src/main/java/org/mabs/MedicalAppointmentBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.mabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentBookingSystemApplication.class, args);
+    }
+
+}
