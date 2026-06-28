08789f6bdc1f0fee12b2b8cf32d205788f5d94e2
phuhocfpt <phulearnse@gmail.com>
2026-06-28 16:37:05 +0700

    fix: Sửa cấu hình jdk để dùng lombok
    - Sửa lại sdk trên máy thành 21 để chạy được lombok
    - Xóa bỏ getter setter -> dùng lombok trong UserRegistrationDto
    - Xóa bỏ direct /error không cần thiết permilAll()

--- MedicalAppointmentBookingSystem/src/main/java/org/mabs/config/SecurityConfig.java
+++ MedicalAppointmentBookingSystem/src/main/java/org/mabs/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/home", "/login", "/register", "/error", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/", "/home", "/login", "/register", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
